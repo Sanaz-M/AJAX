545fb584b98fc05203185205d5f3f7809f5aaea2
martipurull <91315241+martipurull@users.noreply.github.com>
2021-10-27 13:52:41 +0200

    created js file

---
+++ M3-D3.js
@@ -0,0 +1,6 @@
+import { createClient } from 'pexels';
+
+const client = createClient('563492ad6f91700001000001b08d0a9f36b644ac96dfbe0c21553d4e');
+
+// All requests made with the client will be authenticated
+
