f05370a8e2cd0c66060afe0c5c5e6ea92540b89e
martipurull <91315241+martipurull@users.noreply.github.com>
2021-10-27 22:05:45 +0200

    all extras added but one!

--- M3-D3.js
+++ M3-D3.js
@@ -6,6 +6,31 @@
 
 window.onload = () => {
 
+    const showAlert = (numOfImgs) => {
+        const mainContainer = document.querySelector('main')
+        const bootstrapAlertContainer = document.createElement('div')
+        bootstrapAlertContainer.classList.add('alert')
+        bootstrapAlertContainer.classList.add('alert-success')
+        bootstrapAlertContainer.classList.add('text-center')
+        bootstrapAlertContainer.setAttribute('role', 'alert')
+        bootstrapAlertContainer.innerText = `${ numOfImgs } images loaded!`
+        mainContainer.prepend(bootstrapAlertContainer)
+        setTimeout(function () {
+            bootstrapAlertContainer.remove()
+        }, 5000)
+    }
+
+    const showErrorAlert = (errorMessage) => {
+        const mainContainer = document.querySelector('main')
+        const bootstrapAlertContainer = document.createElement('div')
+        bootstrapAlertContainer.classList.add('alert')
+        bootstrapAlertContainer.classList.add('alert-danger')
+        bootstrapAlertContainer.classList.add('text-center')
+        bootstrapAlertContainer.setAttribute('role', 'alert')
+        bootstrapAlertContainer.innerText = `Something went wrong. Namely, this: ${ errorMessage }.`
+        mainContainer.prepend(bootstrapAlertContainer)
+    }
+
     const loadImages = () => {
         fetch("https://api.pexels.com/v1/search?query=forest", {
             method: "GET",
@@ -19,18 +44,56 @@ window.onload = () => {
             })
             .then((body) => {
                 console.log(body)
-                const cardImages = document.querySelectorAll('.card svg')
-                cardImages.forEach(element => element.remove())
+                const cards = document.querySelectorAll('.card:first-child')
+                cards.forEach(element => element.firstElementChild.remove())
                 const cardImagesContainer = document.querySelectorAll('.card')
                 const minsContainers = document.querySelectorAll('small.text-muted')
-                for (let i = 0; i < body.photos.length; i++) {
+                for (let i = 0; i < cardImagesContainer.length; i++) {
                     const newCardImage = document.createElement('img')
                     newCardImage.src = body.photos[i].src.original
                     newCardImage.classList.add('w-100')
                     newCardImage.classList.add('img-height')
                     cardImagesContainer[i].prepend(newCardImage)
                     minsContainers[i].innerText = body.photos[i].id
+                    const modalFade = document.createElement('div')
+                    modalFade.id = 'imageModal'
+                    modalFade.classList.add('modal')
+                    modalFade.classList.add('fade')
+                    modalFade.setAttribute('tabindex', '-1')
+                    modalFade.setAttribute('aria-labelledby', 'imageModalLabel')
+                    modalFade.setAttribute('aria-hidden', 'true')
+                    cardImagesContainer[i].appendChild(modalFade)
+                    const modalDialogue = document.createElement('div')
+                    modalDialogue.classList.add('modal-dialogue')
+                    modalFade.appendChild(modalDialogue)
+                    const modalContent = document.createElement('div')
+                    modalContent.classList.add('modal-content')
+                    modalDialogue.appendChild(modalContent)
+                    const modalHeader = document.createElement('div')
+                    modalHeader.classList.add('modal-header')
+                    modalContent.appendChild(modalHeader)
+                    const modalCloseBtn = document.createElement('button')
+                    modalCloseBtn.type = 'button'
+                    modalCloseBtn.classList.add('close')
+                    modalCloseBtn.setAttribute('data-dismiss', 'modal')
+                    modalCloseBtn.setAttribute('aria-label', 'Close')
+                    modalHeader.appendChild(modalCloseBtn)
+                    const spanInsideModalBtn = document.createElement('span')
+                    spanInsideModalBtn.setAttribute('aria-hidden', 'true')
+                    spanInsideModalBtn.innerText = '&times;'
+                    modalCloseBtn.appendChild(spanInsideModalBtn)
+                    const modalBody = document.createElement('div')
+                    modalBody.classList.add('modal-body')
+                    modalContent.appendChild(modalBody)
+                    const biggerImgForModal = document.createElement('img')
+                    biggerImgForModal.classList.add('img-fluid')
+                    biggerImgForModal.src = body.photos[i].src
+
                 }
+                showAlert(cardImagesContainer.length)
+            })
+            .catch((error) => {
+                showErrorAlert(error)
             })
     }
 
@@ -48,7 +111,7 @@ window.onload = () => {
                 const cards = document.querySelectorAll('.card:first-child')
                 cards.forEach(element => element.firstElementChild.remove())
                 const minsContainers = document.querySelectorAll('small.text-muted')
-                for (let i = 0; i < body.photos.length; i++) {
+                for (let i = 0; i < cards.length; i++) {
                     const newCardImage = document.createElement('img')
                     newCardImage.src = body.photos[i].src.original
                     newCardImage.classList.add('w-100')
@@ -56,6 +119,10 @@ window.onload = () => {
                     cards[i].prepend(newCardImage)
                     minsContainers[i].innerText = body.photos[i].id
                 }
+                showAlert(cards.length)
+            })
+            .catch((error) => {
+                showErrorAlert(error)
             })
     }
 
@@ -78,7 +145,7 @@ window.onload = () => {
                 const cards = document.querySelectorAll('.card:first-child')
                 cards.forEach(element => element.firstElementChild.remove())
                 const minsContainers = document.querySelectorAll('small.text-muted')
-                for (let i = 0; i < body.photos.length; i++) {
+                for (let i = 0; i < cards.length; i++) {
                     const newCardImage = document.createElement('img')
                     newCardImage.src = body.photos[i].src.original
                     newCardImage.classList.add('w-100')
@@ -86,19 +153,70 @@ window.onload = () => {
                     cards[i].prepend(newCardImage)
                     minsContainers[i].innerText = body.photos[i].id
                 }
+                showAlert(cards.length)
+            })
+            .catch((error) => {
+                showErrorAlert(error)
             })
     }
 
 
+    const addCarousel = () => {
+        fetch('https://api.pexels.com/v1/search?query=lakes', {
+            method: "GET",
+            headers:
+                { Authorization: "563492ad6f91700001000001b08d0a9f36b644ac96dfbe0c21553d4e" }
+        })
+            .then((response) => {
+                return response.json()
+            })
+            .then((body) => {
+                const carouselInner = document.querySelector('.carousel-inner')
+                const firstImage = document.querySelector('.carousel-item.active img')
+                firstImage.src = body.photos[0].src.original
+                for (let i = 1; i < body.photos.length; i++) {
+                    const newCarouselItem = document.createElement('div')
+                    newCarouselItem.classList.add('carousel-item')
+                    const newImg = document.createElement('img')
+                    newImg.classList.add('d-block')
+                    newImg.classList.add('w-100')
+                    newImg.src = body.photos[i].src.original
+                    newCarouselItem.appendChild(newImg)
+                    carouselInner.appendChild(newCarouselItem)
+                }
 
+            })
 
+    }
+    addCarousel()
 
+    // < div class="carousel-item active" >
+    //     <img src="..." class="d-block w-100" alt="...">
+    //   </div>
+    //   <div class="carousel-item">
+    //     <img src="..." class="d-block w-100" alt="...">
+    //   </div>
+    //   <div class="carousel-item">
+    //     <img src="..." class="d-block w-100" alt="...">
+    //   </div>
 
 
     const hideCard = (event) => {
         console.log(event)
-        event.target.parentNode.parentNode.parentNode.parentNode.classList.add('d-none')
+        const greatGreatGrandParent = event.target.parentNode.parentNode.parentNode.parentNode
+        greatGreatGrandParent.classList.add('d-none')
+        const bringBackButton = document.createElement('button')
+        bringBackButton.classList.add('btn')
+        bringBackButton.classList.add('btn-primary')
+        bringBackButton.innerText = 'Bring It Back!'
+        bringBackButton.addEventListener('click', function () {
+            bringBackButton.classList.add('d-none')
+            greatGreatGrandParent.classList.remove('d-none')
+        })
+        greatGreatGrandParent.insertAdjacentElement('beforebegin', bringBackButton)
     }
+
+
 
     const loadImgBtn = document.querySelector('.btn-primary')
     loadImgBtn.addEventListener('click', loadImages)
@@ -110,11 +228,18 @@ window.onload = () => {
     const searchImagesBtn = document.querySelector('form button')
     searchImagesBtn.addEventListener('click', searchImages)
 
-    const hideCardBtn = () => {
+    const addHideCardBtn = () => {
         const btnGroupArr = document.querySelectorAll('.btn-group')
         btnGroupArr.forEach(element => element.lastElementChild.innerText = 'Hide')
         btnGroupArr.forEach(element => element.lastElementChild.addEventListener('click', hideCard))
     }
-    hideCardBtn()
+    addHideCardBtn()
+
+    const addViewBtn = () => {
+        const btnGroupArr = document.querySelectorAll('.btn-group')
+        btnGroupArr.forEach(element => element.firstElementChild.setAttribute('data-toggle', 'modal'))
+        btnGroupArr.forEach(element => element.firstElementChild.setAttribute('data-target', '#imageModal'))
+    }
+    addViewBtn()
 
 }
