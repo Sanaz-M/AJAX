85750cfee6dd04e3ff86d1565be114655a92f8fd
martipurull <91315241+martipurull@users.noreply.github.com>
2021-10-27 16:52:22 +0200

    all exercises completed

--- M3-D3.js
+++ M3-D3.js
@@ -61,6 +61,40 @@ window.onload = () => {
 
 
 
+
+    const searchImages = () => {
+        const searchInputValue = document.querySelector('form input').value
+        fetch(`https://api.pexels.com/v1/search?query=${ searchInputValue }`, {
+            method: "GET",
+            headers:
+                { Authorization: "563492ad6f91700001000001b08d0a9f36b644ac96dfbe0c21553d4e" }
+
+        })
+            .then((response) => {
+                return response.json()
+            })
+            .then((body) => {
+                console.log(body)
+                const cards = document.querySelectorAll('.card:first-child')
+                cards.forEach(element => element.firstElementChild.remove())
+                const minsContainers = document.querySelectorAll('small.text-muted')
+                for (let i = 0; i < body.photos.length; i++) {
+                    const newCardImage = document.createElement('img')
+                    newCardImage.src = body.photos[i].src.original
+                    newCardImage.classList.add('w-100')
+                    newCardImage.classList.add('img-height')
+                    cards[i].prepend(newCardImage)
+                    minsContainers[i].innerText = body.photos[i].id
+                }
+            })
+    }
+
+
+
+
+
+
+
     const hideCard = (event) => {
         console.log(event)
         event.target.parentNode.parentNode.parentNode.parentNode.classList.add('d-none')
@@ -72,6 +106,9 @@ window.onload = () => {
 
     const loadSecImgBtn = document.querySelector('.btn-secondary')
     loadSecImgBtn.addEventListener('click', loadSecondaryImages)
+
+    const searchImagesBtn = document.querySelector('form button')
+    searchImagesBtn.addEventListener('click', searchImages)
 
     const hideCardBtn = () => {
         const btnGroupArr = document.querySelectorAll('.btn-group')
